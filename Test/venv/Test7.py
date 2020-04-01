013fc723b8a0cf4b3cf422fd27f20da7a8f9c396
Avimitin <avimitin@gmail.com>
2020-04-01 21:26:53 +0800

    update yaml support

---
+++ Test/venv/Test7.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# author: Avimitin
+# datetime: 2020/4/1 16:57
+import yaml
+test_dic = {}
+with open('test.yml','r+') as test_file:
+    test_dic = yaml.load(test_file,Loader=yaml.FullLoader)
+print(str(test_dic))
+print(test_dic["phone"][0])
