f625a6f85dd6eb67861a90a3a10ad2077ca2d59e
Avimitin <avimitin@gmail.com>
2020-04-29 17:32:05 +0800

    爬虫豆瓣

--- Test/venv/Test13.py
+++ Test/venv/Test13.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 # author: Avimitin
 # datetime: 2020/4/27 17:14
-from urllib import request
-import json
+# 爬虫豆瓣搜索小丑。
+import requests
+from bs4 import BeautifulSoup
 
+header = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.24 ' \
+         'Safari/537.36 Edg/83.0.478.18 '
 
-# 利用urllib读取JSON，然后将JSON解析为Python对象
-def fetch_data(url):
-    with request.urlopen(url) as f:
-        data = f.read()
-        json.loads(data)
-        return data
+r = requests.get('https://www.douban.com/search', params={'q': '小丑'}, headers={'User-Agent': header})
+soup = BeautifulSoup(r.text, 'html5lib')
+
+if __name__ == '__main__':
+    result = soup.find(class_='title')
+    name = result.h3.a.string
+    tag = result.h3.span.string
+    rating = result.find(class_='rating_nums').string
+    details = result.find(class_='subject-cast').string
+    link = result.h3.a['href']
+    print(name, tag, rating, details, link)
